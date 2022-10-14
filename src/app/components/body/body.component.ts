5e6a6c4a9768e12387bf9b56f67011cdeb23a74f
moritz439 <moritz.hipper@gmail.com>
2022-10-14 07:49:01 +0200

    beauty

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -1,3 +1,4 @@
+import { trigger, transition, query, stagger, animate, style } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { BeatCoreService } from 'src/app/services/beat-core.service';
 import { Beat } from 'src/models';
@@ -5,16 +6,28 @@ import { Beat } from 'src/models';
 @Component({
   selector: 'app-body',
   templateUrl: './body.component.html',
-  styleUrls: ['./body.component.scss']
+  styleUrls: ['./body.component.scss'],
+  animations: [
+    trigger('listAnimation', [
+      transition('* <=> *', [
+        query(':enter',
+          [style({ opacity: 0 }), stagger('60ms', animate('600ms ease-out', style({ opacity: 1 })))],
+        )
+      ])
+    ])
+  ]
+
 })
 export class BodyComponent implements OnInit {
 
   beatList: Beat[];
+  beatListUnwanted: Beat[];
   types: string[];
   selectedTypes: string[];
 
   constructor(private beatCore: BeatCoreService) {
     this.beatList = beatCore.beatList;
+    this.beatListUnwanted = [];
     this.selectedTypes = [];
     this.types = [...new Set(this.beatList.flatMap((v) => v.attributes))];
   }
@@ -36,6 +49,16 @@ export class BodyComponent implements OnInit {
     } else {
       this.selectedTypes = this.selectedTypes.filter(typeEl => type !== typeEl)
     }
+
+    if (this.selectedTypes.length === 0) {
+      this.beatList = this.beatCore.beatList;
+      this.beatListUnwanted = [];
+    } else {
+      this.beatList = this.beatCore.beatList.filter(beat => beat.attributes.some(attr => this.selectedTypes.includes(attr)))
+      this.beatListUnwanted = this.beatCore.beatList.filter(beat => !beat.attributes.some(attr => this.selectedTypes.includes(attr)))
+    }
+
+    window.scrollTo({top: 1000, behavior: 'smooth'})
   }
 
 }
