b969cfc68b658cc561b8421fa2d2546a776ca655
moritz439 <moritz.hipper@gmail.com>
2022-10-24 20:22:50 +0200

    Auto-generated commit [ci skip]

--- assets/beats.ts
+++ assets/beats.ts
@@ -6,27 +6,31 @@ export const beats: Beat[] = [
         attributes: ['happy', 'dark'],
         filePath: 'assets/beats/xxx.mp3',
         bpm: 120,
-        key: 'F# minor'
+        key: 'F# minor',
+        date: '24.10.2022'
     },
     {
         name: 'dark atmos',
         attributes: ['deep', 'dark'],
-        filePath: 'assets/beats/xxx.mp3'
-
+        filePath: 'assets/beats/xxx.mp3',
+        date: '22.05.2020'
     },
     {
         name: 'summer day',
         attributes: ['slow', 'techno'],
-        filePath: 'assets/beats/xxx.mp3'
+        filePath: 'assets/beats/xxx.mp3',
+        date: '03.06.2019'
     },
     {
         name: 'winter day',
         attributes: ['happy', 'techno'],
-        filePath: 'assets/beats/xxx.mp3'
+        filePath: 'assets/beats/xxx.mp3',
+        date: '31.01.2020'
     },
     {
         name: 'noch ein beat',
         attributes: ['happy', 'techno'],
-        filePath: 'assets/beats/xxx.mp3'
+        filePath: 'assets/beats/xxx.mp3',
+        date: '12.08.2021'
     }
 ];
