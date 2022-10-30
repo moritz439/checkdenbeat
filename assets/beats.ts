333024595862b6b1338d51401a9ea7f7c6210c39
moritz439 <moritz.hipper@gmail.com>
2022-10-30 15:36:57 +0100

    Auto-generated commit [ci skip]

--- assets/beats.ts
+++ assets/beats.ts
@@ -1,36 +1,78 @@
 import { Beat } from "src/models";
 
-export const beats: Beat[] = [
+
+
+export const beats: Beat[] = [  
     {
-        name: 'Flutes',
-        attributes: ['happy', 'dark'],
-        filePath: 'assets/beats/xxx.mp3',
-        bpm: 120,
-        key: 'F# minor',
+        name: 'bliss',
+        attributes: ['happy', 'chill'],
+        filePath: 'assets/beats/bliss.mp3',
         date: '24.10.2022'
     },
     {
-        name: 'dark atmos',
-        attributes: ['deep', 'dark'],
-        filePath: 'assets/beats/xxx.mp3',
+        name: 'calm down my friend',
+        attributes: ['chill'],
+        filePath: 'assets/beats/calm_down_my_friend.mp3',
         date: '22.05.2020'
     },
     {
-        name: 'summer day',
-        attributes: ['slow', 'techno'],
-        filePath: 'assets/beats/xxx.mp3',
+        name: 'chill',
+        attributes: ['chill', 'dark'],
+        filePath: 'assets/beats/chill.mp3',
         date: '03.06.2019'
     },
     {
-        name: 'winter day',
-        attributes: ['happy', 'techno'],
-        filePath: 'assets/beats/xxx.mp3',
+        name: 'destroyer of worlds',
+        attributes: ['hard', 'aggressive'],
+        filePath: 'assets/beats/destroyer_of_worlds.mp3',
         date: '31.01.2020'
     },
     {
-        name: 'noch ein beat',
-        attributes: ['happy', 'techno'],
-        filePath: 'assets/beats/xxx.mp3',
+        name: 'flutes',
+        attributes: ['hard', 'melodic'],
+        filePath: 'assets/beats/flutes.mp3',
+        date: '12.08.2021'
+    },
+    {
+        name: 'gtr',
+        attributes: ['chill', 'melodic'],
+        filePath: 'assets/beats/gtr.mp3',
+        date: '12.08.2021'
+    },
+    {
+        name: 'heist',
+        attributes: ['hard', 'melodic'],
+        filePath: 'assets/beats/heist.mp3',
+        date: '12.08.2021'
+    },
+    {
+        name: 'ice',
+        attributes: ['boombap', 'melodic'],
+        filePath: 'assets/beats/ice.mp3',
+        date: '12.08.2021'
+    },
+    {
+        name: 'iyf',
+        attributes: ['hard', 'aggressive'],
+        filePath: 'assets/beats/iyf.mp3',
+        date: '12.08.2021'
+    },
+    {
+        name: 'mutter',
+        attributes: ['boombap', 'melodic'],
+        filePath: 'assets/beats/mutter.mp3',
+        date: '12.08.2021'
+    },
+    {
+        name: 'war',
+        attributes: ['melancholic', 'melodic'],
+        filePath: 'assets/beats/war.mp3',
+        date: '12.08.2021'
+    },
+    {
+        name: 'waves',
+        attributes: ['chill', 'melodic'],
+        filePath: 'assets/beats/waves.mp3',
         date: '12.08.2021'
     }
 ];
